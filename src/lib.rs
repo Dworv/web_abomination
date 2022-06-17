e77ce94a99cc93af5c4e5763423a905b1adc709c
Dworv <dwarvyt@gmail.com>
2022-06-16 22:21:27 -0700

    feat: made handle_connection return result to remove unwraps

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,23 @@ pub fn start_server(addr: &str) -> std::io::Result<()> {
     let listener = TcpListener::bind(addr)?;
 
     for stream in listener.incoming() {
-        handle_connection(&mut stream?);
+        handle_connection(&mut stream?)?;
     }
 
     Ok(())
 }
 
-fn handle_connection(stream: &mut TcpStream) {
+fn handle_connection(stream: &mut TcpStream) -> std::io::Result<()> {
     let mut buffer = [0; 1024];
 
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer)?;
 
     println!("{}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+
+    stream.write(response.as_bytes())?;
+    stream.flush()?;
+
+    Ok(())
 }
